9ecc9d383964e94ad695ac9de2f63a051a165e1b
hwahee <pjh5272@gmail.com>
2022-01-21 12:10:17 +0900

    지도 구현

--- src/features/GUI/GUI.tsx
+++ src/features/GUI/GUI.tsx
@@ -1,4 +1,5 @@
 import _ from "lodash"
+import mapboxgl, { LngLat, LngLatLike } from "mapbox-gl"
 import { useEffect, useRef, useState } from "react"
 import { useSelector } from "react-redux"
 import { useAppSelector } from "../../app/hooks"
@@ -6,11 +7,12 @@ import { RootState } from "../../app/store"
 import { selectCount } from "../counter/counterSlice"
 import { IGeojson, IGeojsonFeature } from "../geojson/Geojson"
 import { selectGeojson } from "../geojson/geojsonSlice"
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 const Textbar = (props: { value: string | number, readOnly?: boolean }) => {
     const [val, setVal] = useState(props.value)
     const state = useAppSelector(selectGeojson)
-    
+
     useEffect(() => {
         setVal(i => props.value)
     }, [state])
@@ -19,6 +21,29 @@ const Textbar = (props: { value: string | number, readOnly?: boolean }) => {
         <input type={"text"} value={val} onChange={(e) => { setVal(() => e.target.value) }} readOnly={props.readOnly} />
     )
 }
+const GUIMap = (props: { coordinates: LngLatLike }) => {
+    useEffect(() => {
+        mapboxgl.accessToken = process.env.REACT_APP_MAPBOXGL_ACCESS_TOKEN?.slice(1, process.env.REACT_APP_MAPBOXGL_ACCESS_TOKEN.length - 2) ?? "";
+        const map = new mapboxgl.Map({
+            container: 'map', // container ID
+            style: 'mapbox://styles/mapbox/streets-v11', // style URL
+            center: props.coordinates, // starting position [lng, lat]
+            zoom: 14 // starting zoom
+        });
+
+        const marker = new mapboxgl.Marker()
+            .setLngLat(map.getCenter())
+            .addTo(map);
+
+        return () => {
+            map.remove()
+        }
+    }, [])
+    return (
+        <div id="map" style={{ position: "absolute", width:"480px", minWidth: "240px", minHeight: "120px", maxWidth: "480px", maxHeight: "240px" }}></div>
+    )
+}
+
 
 const GUIFeature = (props: IGeojsonFeature) => {
     return (
@@ -47,6 +72,10 @@ const GUIFeature = (props: IGeojsonFeature) => {
             <tr>
                 <th>image: </th>
                 <td><img src={props.properties.imageUrl ?? "#"} alt={props.properties.imageUrl ?? "no image"} style={{ maxWidth: "480px", maxHeight: "360px" }} /></td>
+            </tr>
+            <tr>
+                <th>map: </th>
+                <td><GUIMap coordinates={props.geometry.coordinates} /></td>
             </tr>
         </>
     )
